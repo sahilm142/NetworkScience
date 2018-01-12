33364b425afe0d061baa4d75f84905f3fb7b2ac1
Sahil Mansoori <sahil.mansoori.143@gmail.com>
2018-01-13 00:21:18 +0530

    Add files via upload

---
+++ simple_lin_regress.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 17:31:35 2018
+
+@author: Sahil
+"""
+# Simple Linear Regression
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
+
+
+# Fitting SLR  to the training set
+## Standarisation before regression (x-x_)/l2-norm
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predicting the test set results
+Y_pred = regressor.predict(X_test)
+
+# Visualising the Training set results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, Y_test, color = 'red')
+# Will get the same regressor line 'already trained'
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
